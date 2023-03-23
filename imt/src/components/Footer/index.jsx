78eaad05051475f931e62ae63d3fad8f56cd0ac1
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2023-03-23 18:11:19 +0000

    linkedin

--- imt/src/components/Footer/index.jsx
+++ imt/src/components/Footer/index.jsx
@@ -4,10 +4,7 @@ import './styles.css'
 const Footer = () => {
     return (
         <footer>
-            <p>made with ❤️ by Marcelo Alves</p> - 
-            <p>Linkedin: 
-                <a href="https://www.linkedin.com/in/malvesgo/" target="_blank" rel="noreferrer"> Marcelo Alves</a>
-            </p>
+            <p>made with ❤️ by <a href="https://pt.linkedin.com/in/malvesgo" target="_blank" rel="noreferrer"> Marcelo Alves</a></p> 
         </footer>
     )
 }
