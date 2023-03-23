53aabbe9c84261aaf93f97dbc561d0854a508fc1
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2023-03-23 16:45:22 +0000

    responsive

--- imt/src/components/Menu/index.jsx
+++ imt/src/components/Menu/index.jsx
@@ -4,25 +4,25 @@ import './styles.css'
 const Menu = () => {
   return (
     <ul className='menu'>
-        <li className='item'>
-          O IMT
-        </li>
-        <li className='item'>
-          IMT ONLINE
-        </li>
-        <li className='item'>
-          VALIDACAO
-        </li>
-        <li className='item'>
-          LICENCIAMENTO
-        </li>
-        <li className='item'>
-          SIMULADORES
-        </li>
-        <li className='item'>
-          MATRICULAS
-        </li>
-      </ul>
+      <li className='item'>
+        <p>O IMT</p>
+      </li>
+      <li className='item'>
+        <p>IMT Online</p>
+      </li>
+      <li className='item'>
+        <p>Validação</p>
+      </li>
+      <li className='item'>
+        <p>Licenciamento</p>
+      </li>
+      <li className='item'>
+        <p>Simuladores</p>
+      </li>
+      <li className='item'>
+        <p>Matrículas</p>
+      </li>
+    </ul>
   )
 }
 
